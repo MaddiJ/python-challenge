71b7e74db000b8b929b3ed457d7db74e56607aba
Joy <125432207+MaddiJ@users.noreply.github.com>
2023-04-06 15:29:59 -0400

    Update Pybank.py

--- Pybank.py
+++ Pybank.py
@@ -1,24 +1,17 @@
 # import file
-import os, csv 
-import pathlib 
-import path
-
-# declare the location for file using Pathlib
-
-input_file= path("Module3Python", "Starter_Code ","Resources","budget_data.csv")
-csvpath = "./Resources/budget_data.csv"
-
+import os
+import csv
+input_file  = r"C:\Users\jmsmi\OneDrive\Documents\School\Python\Starter_Code (5)\Starter_Code\PyBank\Resources\budget_data.csv"
+csvpath = os.path.join('..','Resources','budget_data.csv')
 #list your variables
-total_months=[]
-total_profit= []
-monthlyprofitchange= [] 
-
-csvpath = "./Resources/budget_data.csv"
+# Create empty lists to iterate through specific rows for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
 # Open csv in default read mode with context manager
-with open(csvpath, "r") as csvfile:
-
+with open(input_file,newline="", encoding="utf-8") as budget:
      # Store the contents of budget_data.csv in the variable csvreader
-    csvreader = csv.reader(csvfile,delimiter=",")
+    csvreader = csv.reader(budget,delimiter=",")
     # Skip the header labels to iterate with the values
     header = next(csvreader)
     # Iterate through the rows in the stored file contents
@@ -29,39 +22,28 @@ with open(csvpath, "r") as csvfile:
     # Iterate through the profits in order to get the monthly change in profits
     for i in range(len(total_profit)-1):
         # Take the difference between two months and append to monthly profit change
-        monthlyprofitchange.append(total_profit[i+1]-total_profit[i])
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 # Obtain the max and min of the the montly profit change list
-max_increase_value = max(monthlyprofitchange)
-max_decrease_value = min(monthlyprofitchange)
-# Correlate max and min to the proper month using month list and index from max and min
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
+
+# Correlate max and min to the proper month accordingly using month list and index from max and min
 #We use the plus 1 at the end since month associated with change is the + 1 month or next month
-max_increase_month = monthlyprofitchange.index(max(monthlyprofitchange)) + 1
-max_decrease_month = monthlyprofitchange.index(min(monthlyprofitchange)) + 1
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
 #Print Statements
-# Print Statements
 print("Financial Analysis")
-print("-------------------------")
+print("-----------------------------")
 print(f"Total Months: {len(total_months)}")
 print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthlyprofitchange)/len(monthlyprofitchange),2)}")
 print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
 print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 #Output files
-# Output files
-output_file = "./Analysis/budget_analysis.csv"
-with open(output_file,"w") as file:
-#Write methods to print to Financial_Analysis_Summary
-# Write methods to print to Financial_Analysis_Summary
-    file.write("Financial Analysis")
-    file.write("\n")
-    file.write("-------------------------")
-    file.write("\n")
-    file.write(f"Total Months: {len(total_months)}")
-    file.write("\n")
-    file.write(f"Total: ${sum(total_profit)}")
-    file.write("\n")
-    file.write(f"Average Change: {round(sum(monthlyprofitchange)/len(monthlyprofitchange),2)}")
-    file.write("\n")
-    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-    file.write("\n")
-    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+output_file = r"C:\Users\jmsmi\OneDrive\Desktop\Projects\python-challenge\analysis"
+with open(output_file, "w") as file:
+    file.write("Analysis\n")
+    file.write("-----------------------------\n")
+    file.write(f"Total Months: {len(total_months)}\n")
+    file.write(f"Total: ${sum(total_profit)}\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})\n")
